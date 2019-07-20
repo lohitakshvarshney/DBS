96682a7757042e2bf774486152977b3aba797cc8
I318412 <lohitaksh.varshney@sap.com>
2019-07-20 14:22:11 +0530

    [Code]

---
+++ QcrCode/QcrCode/src/test/java/com/DBS/QcrCode/QcrCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.DBS.QcrCode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QcrCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
